7d31bb3a6c2f186cd53faaeee9567ac37f708df1
aisstana <aissatnabil91@gmail.com>
2024-10-04 15:20:50 +0200

    Feat_01 : vuetify installation and router config

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = defineConfig({
   devServer: {
     port: 3000,
   },
+
   transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		},
+  },
 });
