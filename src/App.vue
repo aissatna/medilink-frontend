0b65992db73b4df3f9b38b80f59199183bb99904
aisstana <aissatnabil91@gmail.com>
2024-10-05 23:02:28 +0200

    Feat_02: init home page and page navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <v-app>
-    <v-main fluid>
+  <v-app class="bg-grey-lighten-4">
+    <!-- show the navbar only if not on login page -->
+    <AppNavbar v-if="showNavbar" />
+    <v-main fluid class="mx-4">
       <router-view>
       </router-view>
     </v-main>
@@ -8,12 +10,20 @@
 </template>
 
 <script>
-
+import AppNavbar from '@/components/AppNavbar.vue';
 export default {
-  name: 'App',
 
+  name: 'App',
+  components: {
+    AppNavbar,
+  },
   data: () => ({
-    //
+
   }),
+  computed: {
+    showNavbar() {
+      return this.$route.path !== '/';
+    },
+  },
 };
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="login">
-    <h1>This is a login page</h1>
+  <div class="d-flex flex-column justify-center align-center login">
+    <h1 class="text-center">This is a login page (DEV IN PROGRESS)</h1>
+    <RouterLink to="/home">
+      <v-btn color="success">Login</v-btn>
+    </RouterLink>
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.login {
+  min-height: 100vh;
+}
+</style>
